173ba49f33253799fa14ce88830d251ec130f2a3
michalmarchwiak <michalmarchwiak@MacBook-Air-Micha.local>
2025-01-19 12:05:24 +0100

    Są 3 pliki: 1. classes, definiuje nasze klasy: defensywnego i ofensywnego piłkarza oraz piłkę i ich właściwości 2. animation, rysuje animacje do symulacji ataku 3. optimalisation, przeprowadza 100 symulacji dla kazdych stalych, przeprowadza odwrotna metodę gradientu prostego aby znaleźć maksimum naszej funkcji czyli procent sukcesów obrońców

--- classes.py
+++ classes.py
@@ -1,28 +1,19 @@
 import numpy as np
 import random
 
-
-
 class OffensivePlayer:
     def __init__(self, name, x:int, y, ideal_x, ideal_y, has_ball=False)->None:
-        """
-        Inicjalizacja zawodnika ofensywnego.
-        """
         self.name = name
-        self.initial_x = x  # Zapisanie początkowej pozycji
+        self.initial_x = x
         self.initial_y = y
         self.x = self.initial_x
         self.y = self.initial_y
-        self.ideal_x = ideal_x  # Idealna pozycja w formacji (X)
-        self.ideal_y = ideal_y  # Idealna pozycja w formacji (Y)
+        self.ideal_x = ideal_x
+        self.ideal_y = ideal_y
         self.has_ball = has_ball
-        self.speed = 1.0  # Prędkość zawodnika
-
+        self.speed = 1.0
 
     def reset_position(self):
-        """
-        Resetuje pozycję zawodnika defensywnego do pozycji początkowej.
-        """
         self.x = self.initial_x
         self.y = self.initial_y
         self.has_ball = False
@@ -31,46 +22,28 @@ class OffensivePlayer:
         return self.name
 
     def move(self, delta_t, defenders):
-        """
-        Poruszanie zawodnika ofensywnego.
-        - Jeśli zawodnik jest na pozycji spalonej, cofa się.
-        - Jeśli zawodnik nie ma piłki, wraca do swojej idealnej pozycji.
-        """
         if self.has_ball:
-            # Znalezienie najbliższego obrońcy bliżej bramki niż zawodnik
             closest_defender_y = min(defender.y for defender in defenders)
-
-            # Sprawdzenie, czy zawodnik jest na pozycji spalonej
             if self.y < closest_defender_y:
-                # Cofanie się w kierunku piłki (lub środka boiska)
-                target_y = min(closest_defender_y - 2, 105 / 2)  # Ustal cel w bezpiecznej odległości od linii spalonego
-                direction = np.arctan2(target_y - self.y, 0)  # Ruch w pionie
+                target_y = min(closest_defender_y - 2, 105 / 2)
+                direction = np.arctan2(target_y - self.y, 0)
                 self.y += self.speed * np.sin(direction) * delta_t
             else:
-                # Ruch w stronę bramki przeciwnika
                 self.y -= self.speed * delta_t
         else:
-            # Jeśli zawodnik nie ma piłki, wraca do swojej idealnej pozycji
             direction = np.arctan2(self.ideal_y - self.y, self.ideal_x - self.x)
-            self.x += self.speed * np.cos(direction) * delta_t * 0.5 # Wolniejszy ruch do idealnej pozycji
+            self.x += self.speed * np.cos(direction) * delta_t * 0.5
             self.y += (-delta_t * 0.75) + (self.speed * np.sin(direction) * delta_t * 0.5)
 
-
     def closest_defender_distance(self, defenders):
-        """
-        Znajdź najbliższego obrońcę i zwróć odległość.
-        """
         distances = [np.linalg.norm((self.x - d.x, self.y - d.y)) for d in defenders]
         return min(distances)
 
     def find_best_teammate(self, teammates, defenders):
-        """
-        Znajdź kolegę z drużyny, który jest najdalej od najbliższego obrońcy.
-        """
         best_teammate = None
         max_distance = -np.inf
         for teammate in teammates:
-            if teammate != self:  # Nie podawaj do siebie
+            if teammate != self:
                 distance_to_closest_defender = teammate.closest_defender_distance(defenders)
                 if distance_to_closest_defender > max_distance:
                     max_distance = distance_to_closest_defender
@@ -81,120 +54,68 @@ class OffensivePlayer:
         best_teammate = self.find_best_teammate(teammates, defenders)
         if best_teammate:
             self.has_ball = False
-            ball.owner = None  # Piłka przestaje mieć właściciela podczas ruchu
-            ball.target = best_teammate  # Ustawienie celu podania
-            ball.is_moving = True  # Piłka zaczyna się poruszać
+            ball.owner = None
+            ball.target = best_teammate
+            ball.is_moving = True
 
 class DefensivePlayer:
     def __init__(self, x, y, ideal_x, ideal_y):
-        """
-        Inicjalizacja zawodnika defensywnego.
-        """
-        self.initial_x = x  # Zapisanie początkowej pozycji
+        self.initial_x = x
         self.initial_y = y
         self.x = self.initial_x
         self.y = self.initial_y
-        self.ideal_x = ideal_x  # Idealna pozycja w formacji
+        self.ideal_x = ideal_x
         self.ideal_y = ideal_y
-        self.speed = 1.1  # Prędkość zawodnika
+        self.speed = 1.1
         self.has_ball = False
 
     def reset_position(self):
-        """
-        Resetuje pozycję zawodnika defensywnego do pozycji początkowej.
-        """
         self.x = self.initial_x
         self.y = self.initial_y
         self.has_ball = False
 
     def closest_offensive_distance(self, offs):
-        """
-        Znajdź najbliższego obrońcę i zwróć odległość.
-        """
         distances = [np.linalg.norm((self.x - o.x, self.y - o.y)) for o in offs]
         return min(distances)
 
     def move_towards(self, target_x, target_y, delta_t):
-        """
-        Poruszaj się w kierunku celu.
-        """
         direction = np.arctan2(target_y - self.y, target_x - self.x)
         self.x += self.speed * np.cos(direction) * delta_t
         self.y += self.speed * np.sin(direction) * delta_t
 
     def calculate_total_force(self, offs, teammates, k_goal, k_opp, k_team, epsilon=5e-1):
-        """
-        Oblicza całkowitą siłę działającą na zawodnika, uwzględniając wpływ wszystkich przeciwników.
-
-        Parametry:
-            offs (list of OffensivePlayer): Lista zawodników ofensywnych (przeciwników).
-            teammates (list of DefensivePlayer): Lista kolegów z drużyny.
-            k_goal (float): Współczynnik siły dążenia do celu.
-            k_opp (float): Współczynnik siły przyciągania do przeciwników.
-            k_team (float): Współczynnik siły odpychania między zawodnikami.
-            epsilon (float): Mała wartość zapobiegająca dzieleniu przez zero (domyślnie 1e-3).
-
-        Zwraca:
-            tuple: Wektor całkowitej siły działającej na zawodnika (F_x, F_y).
-        """
-        # Aktualna pozycja zawodnika
         r_x, r_y = self.x, self.y
-
-        # Idealna pozycja zawodnika
         r_goal_x, r_goal_y = self.ideal_x, self.ideal_y
-
-        # Siła dążenia do idealnej pozycji (celu)
         f_goal_x = -k_goal * (r_x - r_goal_x)
         f_goal_y = -k_goal * (r_y - r_goal_y)
-
-        # Siła przyciągania do wszystkich napastników
         f_opp_x, f_opp_y = 0.0, 0.0
         for opponent in offs:
             r_opp_x, r_opp_y = opponent.x, opponent.y
             r_diff_opp_x = r_opp_x - r_x
             r_diff_opp_y = r_opp_y - r_y
             distance_opp = max((r_diff_opp_x ** 2 + r_diff_opp_y ** 2) ** 0.5, epsilon)
-
-            # Siła malejąca z kwadratem odległości (lub inną funkcją, np. liniową)
             f_opp_x += (k_opp * r_diff_opp_x) / (distance_opp ** 2 + epsilon)
             f_opp_y += (k_opp * r_diff_opp_y) / (distance_opp ** 2 + epsilon)
-
-        # Siła odpychania od kolegów z drużyny
         f_team_x, f_team_y = 0.0, 0.0
         for teammate in teammates:
-            if teammate == self:  # Pomijamy samego siebie
+            if teammate == self:
                 continue
             r_j_x, r_j_y = teammate.x, teammate.y
             r_diff_team_x = r_j_x - r_x
             r_diff_team_y = r_j_y - r_y
             distance_team = max((r_diff_team_x ** 2 + r_diff_team_y ** 2) ** 0.5, epsilon)
-
-            # Siła odpychania malejąca z kwadratem odległości
             f_team_x += (-k_team * r_diff_team_x) / (distance_team ** 2 + epsilon)
             f_team_y += (-k_team * r_diff_team_y) / (distance_team ** 2 + epsilon)
-
-        # Łączna siła
         f_total_x = f_goal_x + f_opp_x + f_team_x
         f_total_y = f_goal_y + f_opp_y + f_team_y
-
         return f_total_x, f_total_y
 
-
     def move(self, offs, teammates, k_goal, k_opp, k_team, delta_t):
-        """
-        Poruszaj się pod wpływem:
-        - Przyciągania do idealnej pozycji.
-        - Przyciągania do piłki (silniejsze, jeśli bliżej piłki).
-        """
         att_x, att_y = self.calculate_total_force(offs, teammates, k_goal, k_opp, k_team)
-
         self.x += att_x * self.speed * delta_t
         self.y += att_y * self.speed * delta_t
 
     def intercept_pass(self, ball):
-        """
-        Sprawdź, czy możesz przeciąć podanie.
-        """
         if ball.is_moving and not ball.owner:
             distance_to_ball = np.linalg.norm((self.x - ball.x, self.y - ball.y))
             if random.random() < 0.5 and distance_to_ball < 0.75:
@@ -202,68 +123,41 @@ class DefensivePlayer:
         return False
 
     def tackle(self, offensive_player):
-        """
-        Sprawdź, czy możesz odebrać piłkę przeciwnikowi.
-        Szansa na odbiór piłki jest odwrotnie proporcjonalna do odległości od obrońcy.
-        Przy odległości 0 wynosi 0.5, a przy odległości większej niż 1.5 wynosi 0.
-        """
-        # Obliczenie odległości do zawodnika ofensywnego
         distance_to_player = np.linalg.norm((self.x - offensive_player.x, self.y - offensive_player.y))
-
-        # Jeśli przeciwnik ma piłkę
         if offensive_player.has_ball:
-            # Wyznaczenie szansy na odbiór piłki
             if distance_to_player <= 1.5:
                 tackle_chance = max(0.5 * (1.5 - distance_to_player) / 1.5, 0)
-                # Rzut kostką na odbiór piłki
                 if random.random() < tackle_chance:
                     offensive_player.has_ball = False
-                    return True  # Piłka została przejęta
-        return False  # Piłka nie została przejęta
-
-
+                    return True
+        return False
 
 class Ball:
     def __init__(self, x, y, owner=None):
-        """
-        Inicjalizacja piłki.
-        """
         self.x = x
         self.y = y
         self.initial_x = self.x
         self.initial_y = self.y
-        self.owner = owner  # Zawodnik posiadający piłkę (lub None)
+        self.owner = owner
         self.is_moving = False
         self.speed = 4.0
         self.target = None
 
-
     def reset(self):
-        """
-        Resetuje pozycję zawodnika defensywnego do pozycji początkowej.
-        """
         self.x = self.initial_x
         self.y = self.initial_y
         self.owner = None
         self.is_moving = False
 
     def closest_defender_distance(self, defenders):
-        """
-        Znajdź najbliższego obrońcę i zwróć odległość.
-        """
         distances = [np.linalg.norm((self.x - d.x, self.y - d.y)) for d in defenders]
         return min(distances)
 
     def update_position(self, delta_t):
-        """
-        Zaktualizuj pozycję piłki podczas podania.
-        """
         if self.is_moving and self.target:
             direction = np.arctan2(self.target.y - self.y, self.target.x - self.x)
             self.x += self.speed * np.cos(direction) * delta_t
             self.y += self.speed * np.sin(direction) * delta_t
-
-            # Sprawdź, czy piłka dotarła do celu
             if np.linalg.norm((self.x - self.target.x, self.y - self.target.y)) < 1:
                 self.is_moving = False
                 self.owner = self.target
---
+++ optimalisation.py
@@ -0,0 +1,191 @@
+from classes import OffensivePlayer, DefensivePlayer, Ball
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+field_x_min, field_x_max = float(0), float(68)
+field_y_min, field_y_max = float(0), float(105)
+
+d1 = DefensivePlayer(18, 15, 18, 18)
+d2 = DefensivePlayer(23, 15, 28, 18)
+d3 = DefensivePlayer(41, 15, 38, 18)
+d4 = DefensivePlayer(48, 15, 48, 18)
+d5 = DefensivePlayer(25, 30, 25, 30)
+d6 = DefensivePlayer(35, 30, 35, 30)
+d7 = DefensivePlayer(45, 30, 45, 30)
+
+o1 = OffensivePlayer("o1", 20, 40, 14, 40, False)
+o2 = OffensivePlayer("o2", 34, 45, 30, 45, True)
+o3 = OffensivePlayer("o3", 48, 40, 52, 40, False)
+o4 = OffensivePlayer("o4", 25, 55, 20, 55, False)
+o5 = OffensivePlayer("o5", 34, 60, 32, 60, False)
+o6 = OffensivePlayer("o6", 43, 55, 46, 55, False)
+
+k_goal = 1.0
+k_opp = 5.0
+k_team = 1.0
+
+ball = Ball(x=34, y=45, owner=o2)
+
+defenders = [d1, d2, d3, d4, d5, d6, d7]
+offensives = [o1, o2, o3, o4, o5, o6]
+
+delta_t = 0.5
+steps = 2000
+
+def find_closest_to_ball(defenders, ball):
+    distances = [(defender, np.linalg.norm((defender.x - ball.x, defender.y - ball.y))) for defender in defenders]
+    closest_defender, min_distance = min(distances, key=lambda x: x[1])
+    return closest_defender
+
+def simulate_step():
+    if ball.is_moving:
+        ball.update_position(delta_t)
+    else:
+        ball.move()
+
+    for offensive in offensives:
+        if offensive.has_ball:
+            offensive.move(delta_t, defenders)
+            closest_defender_distance = offensive.closest_defender_distance(defenders)
+            if closest_defender_distance < 1.5:
+                offensive.pass_ball(ball, offensives, defenders)
+        else:
+            offensive.move(delta_t, defenders)
+
+    for defender in defenders:
+        if ball.is_moving and defender.intercept_pass(ball):
+            ball.is_moving = False
+            ball.owner = defender
+            defender.has_ball = True
+            for offensive in offensives:
+                offensive.has_ball = False
+            return False
+
+    if ball.owner and isinstance(ball.owner, OffensivePlayer):
+        closest_defender = find_closest_to_ball(defenders, ball)
+        success = closest_defender.tackle(ball.owner)
+        if success:
+            ball.owner = closest_defender
+            for offensive in offensives:
+                offensive.has_ball = False
+            closest_defender.has_ball = True
+            return False
+
+    for defender in defenders:
+        if ball.owner is None:
+            defender.move_towards(ball.x, ball.y, delta_t)
+        else:
+            defender.move(offensives, defenders, k_goal, k_opp, k_team, delta_t)
+
+    if ball.y < 16 and ball.owner:
+        return False
+    return True
+
+def reset_simulation():
+    global defenders, offensives, ball
+
+    for defender in defenders:
+        defender.reset_position()
+        defender.has_ball = False
+
+    for offensive in offensives:
+        offensive.reset_position()
+        offensive.has_ball = False
+
+    ball.reset()
+    offensives[1].has_ball = True
+    ball.owner = offensives[1]
+    ball.x, ball.y = offensives[1].x, offensives[1].y
+    ball.is_moving = False
+
+def simulate_multiple_times(num_simulations=100, k_goal=1.0, k_opp=1.0, k_team=1.0):
+    global defenders, offensives, ball
+    for defender in defenders:
+        defender.k_goal = k_goal
+        defender.k_opp = k_opp
+        defender.k_team = k_team
+
+    defender_wins = 0
+
+    for simulation in range(num_simulations):
+        reset_simulation()
+
+        for step in range(steps):
+            if not simulate_step():
+                if ball.owner and isinstance(ball.owner, DefensivePlayer):
+                    defender_wins += 1
+                break
+
+    return defender_wins / num_simulations
+
+def gradient_ascent(num_simulations=100, alpha=0.005, max_iters=100, epsilon=1e-6, decay_rate=1):
+    k_goal, k_opp, k_team = 5, 5, 5
+    results = []
+    best_k_goal, best_k_opp, best_k_team = k_goal, k_opp, k_team
+    best_value = 0.0
+
+    for iteration in range(max_iters):
+        current_value = simulate_multiple_times(num_simulations, k_goal, k_opp, k_team)
+        results.append([k_goal, k_opp, k_team, current_value])
+
+        if current_value > best_value:
+            best_value = current_value
+            best_k_goal, best_k_opp, best_k_team = k_goal, k_opp, k_team
+
+        grad_k_goal = (simulate_multiple_times(num_simulations, k_goal + epsilon, k_opp, k_team) - current_value) / epsilon
+        grad_k_opp = (simulate_multiple_times(num_simulations, k_goal, k_opp + epsilon, k_team) - current_value) / epsilon
+        grad_k_team = (simulate_multiple_times(num_simulations, k_goal, k_opp, k_team + epsilon) - current_value) / epsilon
+
+        grad_norm = np.sqrt(grad_k_goal**2 + grad_k_opp**2 + grad_k_team**2 + 1e-8)
+        grad_k_goal /= grad_norm
+        grad_k_opp /= grad_norm
+        grad_k_team /= grad_norm
+
+        k_goal = max(0.1, min(k_goal + alpha * grad_k_goal, 10.0))
+        k_opp = max(0.1, min(k_opp + alpha * grad_k_opp, 10.0))
+        k_team = max(0.1, min(k_team + alpha * grad_k_team, 10.0))
+
+        alpha *= decay_rate
+
+        print(f"Iteracja {iteration + 1}: k_goal={k_goal:.4f}, k_opp={k_opp:.4f}, k_team={k_team:.4f}, wartość={current_value:.4f}")
+
+        if grad_norm < epsilon:
+            print("Gradient ascent zakończony - osiągnięto zbieżność.")
+            break
+
+    print(f"\nNajlepsza wartość funkcji celu: {best_value:.4f}")
+    print(f"Odpowiadające jej parametry: k_goal={best_k_goal:.4f}, k_opp={best_k_opp:.4f}, k_team={best_k_team:.4f}")
+
+    return best_k_goal, best_k_opp, best_k_team, np.array(results)
+
+optimal_k_goal, optimal_k_opp, optimal_k_team, results = gradient_ascent(num_simulations=100, alpha=1, max_iters=1000)
+
+function_values = results[:, 3]
+
+plt.plot(function_values)
+plt.title("Wartość funkcji celu w kolejnych iteracjach")
+plt.xlabel("Iteracja")
+plt.ylabel("f(x, y, z)")
+plt.show()
+
+def plot_3d_heatmap(results):
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    k_goal = results[:, 0]
+    k_opp = results[:, 1]
+    k_team = results[:, 2]
+    function_values = results[:, 3]
+    norm = plt.Normalize(function_values.min(), function_values.max())
+    colors = cm.get_cmap('RdYlGn')(norm(function_values))
+    scatter = ax.scatter(k_goal, k_opp, k_team, c=function_values, cmap='RdYlGn', s=50, edgecolor='k')
+    cbar = fig.colorbar(scatter, ax=ax, pad=0.1, shrink=0.8)
+    cbar.set_label('Wartość funkcji celu (f(x, y, z))')
+    ax.set_xlabel('k_goal')
+    ax.set_ylabel('k_opp')
+    ax.set_zlabel('k_team')
+    plt.title("Heatmapa przestrzeni parametrów z wartościami funkcji celu")
+    plt.show()
+
+plot_3d_heatmap(results)
